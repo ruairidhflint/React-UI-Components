a27cf55dc9577a17f656f3c7580feef3648aa5a9
Rory Flint <rory@roryflint.co.uk>
2019-05-28 12:18:15 +0100

    MVP Complete (aesthetics)

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+
+function ActionButton(props){
+    const { action, buttonStyle} = props;
+    return(
+        <button className={buttonStyle}>
+            {action}
+        </button>
+    );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+
+function NumberButton(props){
+    const { number, buttonStyle} = props;
+    return(
+        <button className={buttonStyle}>
+            {number}
+        </button>
+    );
+}
+
+export default NumberButton;

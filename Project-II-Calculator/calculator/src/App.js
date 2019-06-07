4e8878c7058708568445eabdbe2fcd5001a28e83
Rory Flint <rory@roryflint.co.uk>
2019-06-07 18:07:35 +0100

    refactored to semi working

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,43 +1,72 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
-import CreateButtons from './components/ButtonComponents/NumberButton.js';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
 import ActionButton from './components/ButtonComponents/ActionButton.js';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: [],
+      operation: []
 
-
-const App = () => {
-  let [myDisplay, updateMyDisplay] = useState(0);
-  
-  function renderNumber(x){
-    updateMyDisplay(myDisplay = x.target.textContent)
+    }
   }
 
-  function setToZero(x){
-    updateMyDisplay(myDisplay = 0)
+  display = (value) => {
 
+    let result = this.state.display.join('')
+
+    if (value === "="){
+      this.setState({
+        display: eval(result)
+      })
+    }
+    else {
+      this.setState({
+        display: this.state.display.concat(value),
+        operation: this.state.display.concat(value)
+      })
+    }
   }
-  
+
+
+
+clear = () => {
+  this.setState({
+    display: []
+  })
+
+}
+
+render() {
   return (
     <div className="calculator">
       <div className="display-row">
-        <CalculatorDisplay displayNumber={myDisplay} /></div>
+        <CalculatorDisplay displayNumber={this.state.display} /></div>
       <div>
-        <ActionButton  onClick={setToZero} action="clear" buttonStyle="largeButton" />
-        <CreateButtons  onClick={renderNumber} links={buttonContent}/>
-  
+        <ActionButton onClick={this.clear} action="clear" buttonStyle="largeButton" />
+
+        {buttonContent.map(button => {
+          return (
+            <NumberButton
+              onClick={this.display}
+              content={button}
+            />
+          )
+        })}
+
       </div>
     </div>
   );
 };
+}
 
-
-
-
- const buttonContent = [{text:"%", buttonStyle:"smallActionButton"}, {text: "7", buttonStyle:"smallButton"},
-{text: "8", buttonStyle:"smallButton"}, {text: "9", buttonStyle:"smallButton"}, {text: "x", buttonStyle:"smallActionButton"},
-{text: "4", buttonStyle:"smallButton"}, {text: "5", buttonStyle:"smallButton"}, {text: "6", buttonStyle:"smallButton"}, {text: "-", buttonStyle:"smallActionButton"},
-{text: "1", buttonStyle:"smallButton"}, {text: "2", buttonStyle:"smallButton"}, {text: "3", buttonStyle:"smallButton"}, {text: "+", buttonStyle:"smallActionButton"},
-{text: "0", buttonStyle:"largeButton"}, {text: "=", buttonStyle:"smallActionButton"}];
+const buttonContent = [{ text: "%", buttonStyle: "smallActionButton", value: "/" }, { text: "7", buttonStyle: "smallButton", value: "7" },
+{ text: "8", buttonStyle: "smallButton", value: "8" }, { text: "9", buttonStyle: "smallButton", value: "9" }, { text: "x", buttonStyle: "smallActionButton", value: "*" },
+{ text: "4", buttonStyle: "smallButton", value: "4" }, { text: "5", buttonStyle: "smallButton", value: "5" }, { text: "6", buttonStyle: "smallButton", value: "6" }, { text: "-", buttonStyle: "smallActionButton", value: "-" },
+{ text: "1", buttonStyle: "smallButton", value: "1" }, { text: "2", buttonStyle: "smallButton", value: "2" }, { text: "3", buttonStyle: "smallButton", value: "3" }, { text: "+", buttonStyle: "smallActionButton", value: "+" },
+{ text: "0", buttonStyle: "largeButton", value: "0" }, { text: "=", buttonStyle: "smallActionButton", value: "=" }];
 
 export default App;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,29 +2,13 @@ import React from 'react';
 import './Button.css';
 
 function NumberButton(props){
-    const { text, buttonStyle, onClick} = props;
-    
 
     return(
-        <button onClick={onClick} className={buttonStyle}>
-            {text}
+        <button onClick={() => props.onClick(props.content.value)} className={props.content.buttonStyle}>
+            {props.content.text}
         </button>
     );
 }
 
-function CreateButtons(props){
-    const  { links, onClick } = props;
-   
-    return (
-        <>
-            {
-             links.map(link => {
-                 return <NumberButton text={link.text} buttonStyle={link.buttonStyle} onClick={onClick}/> 
-             })
-            }
-        </>
-    );
-}
 
-
-export default CreateButtons;
+export default NumberButton;
